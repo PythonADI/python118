43e2613ac72ee4f2ba66197a916a1c792d3eefcd
Luka Tavkhelidze <52665503+tavkhelidzeluka@users.noreply.github.com>
2025-01-19 10:18:17 +0100

    Add workshop 1 code

---
+++ wokrshop_1/main.py
@@ -0,0 +1,15 @@
+money_in_wallet = 100
+sandwich_price = 10
+
+print("I've ", money_in_wallet, "$ in my wallet")
+print("Sandwhich price", sandwich_price, "$")
+
+# print(money_in_wallet - sandwich_price)
+# money_in_wallet = money_in_wallet - sandwich_price
+money_in_wallet -= sandwich_price
+money_in_wallet += 2500
+money_in_wallet *= 4
+money_in_wallet /= 2
+# money_in_wallet / 2
+
+print("I bought a sandwich and now I've got ", money_in_wallet, "$")
