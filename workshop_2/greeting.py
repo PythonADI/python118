38f561ea3686c3cc438c9e696d5f7798055ca61e
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2025-01-21 20:37:01 +0400

    Add greeting code

---
+++ workshop_2/greeting.py
@@ -0,0 +1,9 @@
+first_name = "George"
+last_name = "Washington"
+# full_name = "Nick" + " " + "Washington"
+full_name = first_name + " " + last_name
+greeting = "Hello " + full_name
+print(full_name)
+print(greeting)
+
+# print(is_on)
