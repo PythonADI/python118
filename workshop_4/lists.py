0d28a6bb72371b7fb45fd79300a1d7a621c8fa51
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2025-02-07 19:43:35 +0400

    Change some text

--- workshop_4/lists.py
+++ workshop_4/lists.py
@@ -1,9 +1,9 @@
 temperatures = [11, 14, 13, 5, 6, 7, 8, 9, 10]
 
 print(f'We recorded {len(temperatures)} day temperature')
-print(f'1st day the temperature was: {temperatures[0]} celsius')
+print(f'first day the temperature was: {temperatures[0]} celsius')
 # print(f'1st day the temperature was: {temperatures[len(temperatures) - 1]} celsius')
-print(f'1st day the temperature was: {temperatures[-1]} celsius')
+print(f'first day the temperature was: {temperatures[-1]} celsius')
 
 
 # day {day number} was {temperature} celsius
